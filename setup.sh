18de23d6e0dac0da6d01fb064bb9cf0e19f8c2cd
Fabrice Pont <fa.pont@gmail.com>
2024-03-10 11:04:54 +0100

    add setup

---
+++ setup.sh
@@ -0,0 +1,6 @@
+
+
+# mkdir tmp-install && cd tmp-install
+# sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+
+sudo pacman -S --noconfirm xdg-desktop-portal-hyprland xdg-desktop-portal-gtk qt5-wayland qt6-wayland hyprpaper gammastep zsh unzip man-db alacritty waybar neovim 
